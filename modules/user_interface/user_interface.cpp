df788cce30209b2a93265e836cebb24e4557930d
PAPAN004 <155662184+PAPAN004@users.noreply.github.com>
2024-02-06 12:40:33 -0500

    Update user_interface.cpp

--- modules/user_interface/user_interface.cpp
+++ modules/user_interface/user_interface.cpp
@@ -134,19 +134,19 @@ static void userInterfaceDisplayInit()
     displayInit();
      
     displayCharPositionWrite ( 0,0 );
-    displayStringWrite( "Temperature:" );
+    displayStringWrite( "Tmp:" );
 
-    displayCharPositionWrite ( 0,1 );
+    displayCharPositionWrite ( 9,0 );
     displayStringWrite( "Gas:" );
     
-    displayCharPositionWrite ( 0,2 );
+    displayCharPositionWrite ( 0,1 );
     displayStringWrite( "Alarm:" );
 }
 
 static void userInterfaceDisplayUpdate()
 {
     static int accumulatedDisplayTime = 0;
-    char temperatureString[3] = "";
+    char temperatureString[2] = "";
     
     if( accumulatedDisplayTime >=
         DISPLAY_REFRESH_TIME_MS ) {
@@ -154,20 +154,20 @@ static void userInterfaceDisplayUpdate()
         accumulatedDisplayTime = 0;
 
         sprintf(temperatureString, "%.0f", temperatureSensorReadCelsius());
-        displayCharPositionWrite ( 12,0 );
+        displayCharPositionWrite ( 4,0 );
         displayStringWrite( temperatureString );
-        displayCharPositionWrite ( 14,0 );
+        displayCharPositionWrite ( 6,0 );
         displayStringWrite( "'C" );
 
-        displayCharPositionWrite ( 4,1 );
+        displayCharPositionWrite ( 13,0 );
 
         if ( gasDetectorStateRead() ) {
-            displayStringWrite( "Detected    " );
+            displayStringWrite( "D" );
         } else {
-            displayStringWrite( "Not Detected" );
+            displayStringWrite( "ND" );
         }
 
-        displayCharPositionWrite ( 6,2 );
+        displayCharPositionWrite ( 6,1 );
         
         if ( sirenStateRead() ) {
             displayStringWrite( "ON " );
